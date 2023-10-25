740e963fdf9cb1b1cb57950b9c346903539778b8
Lashagurgenidze1989 <lashagurgenidze1989@gmail.com>
2023-10-25 12:12:00 -0600

    final version

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const daysText = document.getElementById("days");
+const hoursText = document.getElementById("hours");
+const minutesText = document.getElementById("minutes");
+const secondsText = document.getElementById("seconds");
+
 const countDown = setInterval(() => {
   const now = new Date().getTime();
   const targetDate = new Date("2024-04-29T23:59:59").getTime();
@@ -8,8 +13,16 @@ const countDown = setInterval(() => {
   const hours = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
   const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
 
-  document.getElementById("days").innerText = days;
-  document.getElementById("hours").innerText = hours;
-  document.getElementById("minutes").innerText = minutes;
-  document.getElementById("seconds").innerText = seconds;
+  daysText.innerText = days;
+  hoursText.innerText = hours;
+  minutesText.innerText = minutes;
+  secondsText.innerText = seconds;
+
+  if (remainingTime <= 0) {
+    clearInterval(countDown);
+    daysText.innerText = "00";
+    hoursText.innerText = "00";
+    minutesText.innerText = "00";
+    secondsText.innerText = "00";
+  }
 }, 1000);
